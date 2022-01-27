af7544593f1ad4ca57de8045205e70a4c5bc6de4
Jo Hanna Pearce <jo@nrwl.io>
2022-01-27 13:14:33 +0000

    feat(storybook): add storybook stories

---
+++ apps/todos-e2e/src/integration/nx-welcome.component.spec.ts
@@ -0,0 +1,6 @@
+describe('todos', () => {
+  beforeEach(() => cy.visit('/iframe.html?id=nxwelcomecomponent--primary'));
+  it('should render the component', () => {
+    cy.get('todos-nx-welcome').should('exist');
+  });
+});
